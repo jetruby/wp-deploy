3d418f0da640370ef3d0f4ca062f890254d7768d
Ilya Kuchaev <ilya.kuchaev@jetruby.com>
2015-10-01 16:25:16 +0300

    Cohenhanler production deploy configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 ############################################
 
 set :stage, :production
-set :stage_url, "http://www.example.com"
-server "XXX.XXX.XX.XXX", user: "SSHUSER", roles: %w{web app db}
-set :deploy_to, "/deploy/to/path"
+set :stage_url, "http://cohenhandler.com.au"
+server "cohenhandler.com.au", user: "deploy", roles: %w{web app db}
+set :deploy_to, "/var/www/blog/production"
 
 ############################################
 # Setup Git
